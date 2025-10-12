99e6901c4085fcade31e5f3a496088406a750410
VictorMello1993 <victorsmello93@gmail.com>
2025-10-12 15:40:21 -0300

    Listagem de produtos

--- frontend-gestao-marketplace/src/app/pages/products/products.ts
+++ frontend-gestao-marketplace/src/app/pages/products/products.ts
@@ -6,18 +6,25 @@ import { EnumProductStatus } from '../../interfaces/product-status-enum';
 import { CommonModule, DecimalPipe } from '@angular/common';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 registerLocaleData(localePt);
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule, DecimalPipe],
+  imports: [DecimalPipe, ReactiveFormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
 export class Products implements OnInit{
   products: IProductResponse[] = [];
+  filteredProducts: IProductResponse[] = [];
   productStatus = EnumProductStatus;
+
+  filterForm = new FormGroup({
+    title: new FormControl(''),
+    status: new FormControl('')
+  })
 
   private readonly _productsService = inject(ProductsService);
 
@@ -25,8 +32,25 @@ export class Products implements OnInit{
     this._productsService.getProducts().pipe(take(1)).subscribe({
       next: (response) => {
         this.products = response.data
+        this.filteredProducts = response.data
       }
     });
+  }
+
+  filterProducts(){
+    const title = this.filterForm.value.title?.toLowerCase()
+    const status = this.filterForm.value.status?.toLowerCase()
+
+    this.filteredProducts = this.products.filter((product) =>
+    (!title || product.title.toLowerCase().includes(title)) &&
+    (!status || product.status.toLowerCase().includes(status)))
+  }
+
+  clearFilter(){
+    this.filterForm.reset();
+    this.filterForm.get('status')?.setValue('');
+
+    this.filteredProducts = this.products;
   }
 
 }
